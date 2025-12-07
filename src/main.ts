8164b0a3a32bd06dc49a7151df2db74f46ee0b15
hesam-valizadeh <hesam.valizadeh@gmail.com>
2025-12-07 20:33:30 +0330

    21

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { provideZoneChangeDetection } from '@angular/core/types/core';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
+  .catch((err:any) => console.error(err));
